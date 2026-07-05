f918e6b9951218a7af8431bf98eee0ccedbb75d1
Adnan <adnan12sami12@gmail.com>
2026-07-05 10:46:14 +0300

    feat(persistence): add RentalRepository interface for US2.1

---
+++ src/main/java/com/vrms/persistence/RentalRepository.java
@@ -0,0 +1,18 @@
+package com.vrms.persistence;
+
+import com.vrms.domain.Rental;
+
+/**
+ * This interface is responsible for storing rental records.
+ * It defines what any storage method (in-memory, database, etc.)
+ * must be able to do when it comes to saving a rental.
+ */
+public interface RentalRepository {
+
+    /**
+     * Saves a new rental record.
+     *
+     * @param rental the rental record to be saved
+     */
+    void save(Rental rental);
+}
