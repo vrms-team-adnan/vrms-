f35f894db4501623190f482959760d8a90b168b3
Adnan <adnan12sami12@gmail.com>
2026-07-11 04:19:31 +0300

    docs(persistence): complete Javadoc for RentalRepository method

--- src/main/java/com/vrms/persistence/RentalRepository.java
+++ src/main/java/com/vrms/persistence/RentalRepository.java
@@ -8,5 +8,11 @@ import com.vrms.domain.Rental;
  * must be able to do when it comes to saving a rental.
  */
 public interface RentalRepository {
+
+    /**
+     * Saves a new rental record.
+     *
+     * @param   rental   the rental record to be saved
+     */
     void save(Rental rental);
 }
