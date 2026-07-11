e91680e8a2da0e4b11573d8e24fe078f3c0c7a07
Adnan <adnan12sami12@gmail.com>
2026-07-11 04:14:48 +0300

    docs(domain): add Javadoc to VehicleStatus enum

--- src/main/java/com/vrms/domain/VehicleStatus.java
+++ src/main/java/com/vrms/domain/VehicleStatus.java
@@ -1,5 +1,12 @@
 package com.vrms.domain;
 
+/**
+ * Represents the possible states of a vehicle.
+ */
 public enum VehicleStatus {
-AVAILABLE , RENTED
+    /** The vehicle is available for rental. */
+    AVAILABLE,
+
+    /** The vehicle is currently rented. */
+    RENTED
 }
