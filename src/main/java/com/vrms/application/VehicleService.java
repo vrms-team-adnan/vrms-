9f0089a63c54175268c1f4618fb5cfdaca95bcc2
Adnan <adnan12sami12@gmail.com>
2026-07-11 04:17:33 +0300

    docs(application): add Javadoc to VehicleService

--- src/main/java/com/vrms/application/VehicleService.java
+++ src/main/java/com/vrms/application/VehicleService.java
@@ -1,23 +1,37 @@
 package com.vrms.application;
+
 import java.util.ArrayList;
 import java.util.List;
 import com.vrms.domain.Vehicle;
 import com.vrms.persistence.VehicleRepository;
 
+/**
+ * Handles retrieving available vehicles.
+ */
 public class VehicleService {
-	private final VehicleRepository vehicleRepository;
-	public VehicleService(VehicleRepository vehicleRepository) {
-		this.vehicleRepository=vehicleRepository;
-	}
-	
-	public List<Vehicle> getAvailableVehicles(){
-		List<Vehicle> availableVehicles = new ArrayList<>();
-		for(Vehicle vehicle:vehicleRepository.findAll()) {
-			if(vehicle.isAvailable()) {
-				availableVehicles.add(vehicle);
-			}
-		}
-		return availableVehicles;
-	}
+    private final VehicleRepository vehicleRepository;
 
+    /**
+     * Creates a new VehicleService.
+     *
+     * @param   vehicleRepository   the repository used to look up vehicles
+     */
+    public VehicleService(VehicleRepository vehicleRepository) {
+        this.vehicleRepository = vehicleRepository;
+    }
+
+    /**
+     * Returns all vehicles that are currently available for rental.
+     *
+     * @return  a list of available vehicles
+     */
+    public List<Vehicle> getAvailableVehicles() {
+        List<Vehicle> availableVehicles = new ArrayList<>();
+        for (Vehicle vehicle : vehicleRepository.findAll()) {
+            if (vehicle.isAvailable()) {
+                availableVehicles.add(vehicle);
+            }
+        }
+        return availableVehicles;
+    }
 }
