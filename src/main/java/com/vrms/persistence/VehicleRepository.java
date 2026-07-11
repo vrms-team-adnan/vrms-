1b2ea3dc456cd3a587d02245ef52b2b85a261606
Adnan <adnan12sami12@gmail.com>
2026-07-11 04:15:44 +0300

    docs(persistence): add Javadoc to VehicleRepository interface

--- src/main/java/com/vrms/persistence/VehicleRepository.java
+++ src/main/java/com/vrms/persistence/VehicleRepository.java
@@ -1,6 +1,17 @@
 package com.vrms.persistence;
+
 import java.util.List;
 import com.vrms.domain.Vehicle;
+
+/**
+ * Interface for retrieving vehicles.
+ */
 public interface VehicleRepository {
-	List<Vehicle> findAll();
+
+    /**
+     * Returns all vehicles in the repository.
+     *
+     * @return  a list of all vehicles
+     */
+    List<Vehicle> findAll();
 }
